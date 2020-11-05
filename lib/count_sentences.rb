2282017e326cfb494fca6cf25a00d4d6b7de73a2
DariasKehl <neocount@gmail.com>
2020-11-05 12:34:05 -0500

    commit tree 9e690d4976090a37315568817a863133e2ff7eef

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return self.end_with?(".")
   end
 
   def question?
-
+    return self.end_with?("?")
   end
 
   def exclamation?
-
+    return self.end_with?("!")
   end
 
   def count_sentences
-
+    new_array = self.split(/[.?!]/).reject {|string| string.empty?}
+    return new_array.length
   end
 end
